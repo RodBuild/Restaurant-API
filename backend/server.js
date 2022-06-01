4bec859c8546b8c53ab13cfb03ee396c911c0065
Rodrigo Rodriguez <rod19016@byui.edu>
2022-06-01 08:31:23 -0600

    changed format " -> '

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const mongodb = require("./database/connect");
-const bodyParser = require("body-parser"); //npm, for POST requests
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocumentAuto = require("./swagger/swagger-auto.json");
-const swaggerDocument = require("./swagger/swagger.json");
+const express = require('express');
+const mongodb = require('./database/connect');
+const bodyParser = require('body-parser'); //npm, for POST requests
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocumentAuto = require('./swagger/swagger-auto.json');
+const swaggerDocument = require('./swagger/swagger.json');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -15,17 +15,17 @@ app
   .use(bodyParser.json())
   .use('/', require('./routes'));
 
-// 
+//
 process.on('uncaughtException', (err, origin) => {
   console.log(process.stderr.fd, `Caught exception: ${err}\n + Exception origin: ${origin}`);
 });
 
 // now connect to database...
 mongodb.initDb((err) => {
-    if (err) {
-        console.error(err);
-    } else {
-        app.listen(port);
-        console.log(`Connected to DB and listen on port ${port}`);
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listen on port ${port}`);
+  }
 });
