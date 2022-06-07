0e2279c995e2217a668a7dc59f21992d2fbeb36f
Rodrigo Rodriguez <rod19016@byui.edu>
2022-06-06 19:12:07 -0600

    make it better

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -3,17 +3,19 @@ const router = express.Router();
 const controller = require('../controllers/reviews');
 const validation = require('../validation/middleware');
 const { body } = require('express-validator');
+const { requiresAuth } = require('express-openid-connect');
 
 /*GET*/
-router.get('/', controller.getAll);
-router.get('/state/', controller.getReviewState);
-router.get('/:id', controller.getReviewID);
-router.get('/email/:email', controller.getReviewEmail);
+router.get('/', requiresAuth(), controller.getAll);
+router.get('/state/', requiresAuth(), controller.getReviewState);
+router.get('/:id', requiresAuth(), controller.getReviewID);
+router.get('/email/:email', requiresAuth(), controller.getReviewEmail);
 // router.get('/:states', controller.getReviewState);
 
 /*POST*/
 router.post(
   '/',
+  requiresAuth(),
   body('stars', 'Star rating should be between 1-5').isInt({ min: 1, max: 5 }),
   validation.saveReview,
   controller.createReview
@@ -22,19 +24,21 @@ router.post(
 /*PUT*/
 router.put(
   '/:id',
+  requiresAuth(),
   body('stars', 'Star rating should be between 1-5').isInt({ min: 1, max: 5 }),
   validation.saveReview,
   controller.editReviewID
 );
 router.put(
   '/:email/:city/:state',
+  requiresAuth(),
   body('stars', 'Star rating should be between 1-5').isInt({ min: 1, max: 5 }),
   validation.saveReviewShort,
   controller.editReview
 );
 
 /*DELETE*/
-router.delete('/:id', controller.deleteReviewID);
-router.delete('/:email/:city/:state', controller.deleteReview);
+router.delete('/:id', requiresAuth(), controller.deleteReviewID);
+router.delete('/:email/:city/:state', requiresAuth(), controller.deleteReview);
 
 module.exports = router;
